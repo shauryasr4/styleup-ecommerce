9f626a4d670035e1cd72b0c0d470d45a608a9280
Shaurya Rawat <shaurya@smartserv.io>
2019-12-07 16:11:30 +0530

    layout of homepage designed

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './menu-item.style.scss'
+const MenuItem = ({ title, imageUrl, size }) => {
+    return (
+        <div className={`${size} menu-item`}>
+            <div style={{backgroundImage: `url(${imageUrl})`}} className='background-image' />
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'> BUY NOW </span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
