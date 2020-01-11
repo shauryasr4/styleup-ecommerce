d7d6e05c47765208baf3a9d31c6c708dd7db39a3
Shaurya Rawat <shaurya@smartserv.io>
2020-01-11 13:57:02 +0530

    added empty cart icon display

--- README.md
+++ README.md
@@ -66,3 +66,5 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -10,15 +10,18 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.style.scss';
+import { ReactComponent as EmptyCartIcon } from "../../assets/empty-cart.svg";
 
 const CartDropdown = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
+            {cartItems.length > 0 ? 
             <div className='cart-items'>
-                {cartItems.map(function (cartItem) {
-                    return <CartItem item={cartItem} />
-                })}
-            </div>
+            {cartItems.map(function (cartItem) {
+                return <CartItem item={cartItem} />
+            })}
+        </div>: <div className='empty-cart'><EmptyCartIcon className='empty-cart-icon'/><span>Your cart is empty :(</span></div>}
+            
             <CustomButton> Go To Cart </CustomButton>
         </div>
     )
